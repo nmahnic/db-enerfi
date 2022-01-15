9ef097c388e4bd2218a42d0e990d97f23c32ec1c
Nicolas Mahnic <nico.mahnic94@gmail.com>
2022-01-15 19:01:28 -0300

    log 1 ESP

--- launchLoggers.py
+++ launchLoggers.py
@@ -2,13 +2,13 @@ import subprocess
 import multiprocessing
 
 def logger1():
-    subprocess.call("python3 esp32Logger.py 84-D8-1B-0C-5B-C1", shell=True)
+    subprocess.call("python3 esp32Logger.py B0-B2-8F-1D-4D-03", shell=True)
 
 def logger2():
     subprocess.call("python3 esp32Logger.py B0-B2-8F-1D-4D-02", shell=True)
 
 if __name__ == '__main__':
     p1 = multiprocessing.Process(name='p1', target=logger1)
-    p = multiprocessing.Process(name='p', target=logger2)
+    # p = multiprocessing.Process(name='p', target=logger2)
     p1.start()
-    p.start()
+    # p.start()
